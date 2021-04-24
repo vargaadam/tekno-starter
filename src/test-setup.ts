ce83af41deeea6ebc799430ab22370a623d2f8ca
Varga Adam <vargaad95@gmail.com>
2021-04-24 11:32:53 +0200

    deps: add testing packages

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -2,8 +2,12 @@ import sinon from 'sinon';
 import chai from 'chai';
 
 import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
+import deepEqualInAnyOrder from 'deep-equal-in-any-order';
 
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
+chai.use(deepEqualInAnyOrder);
 
 afterEach(() => {
   sinon.restore();

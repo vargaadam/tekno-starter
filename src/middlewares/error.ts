9056116dd72daac9325d385f83767017d29f246c
Varga Adam <vargaad95@gmail.com>
2021-04-24 11:30:07 +0200

    chore(ErrorMiddleware): extend error type

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions';
 
-const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction): void => {
+const errorMiddleware = <T extends HttpException>(error: T, req: Request, res: Response, next: NextFunction): void => {
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
